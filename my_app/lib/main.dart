3eca6090ab10787ed7cad41846b25ec49194d348
Leonardo Lucena <leo.lucena10@hotmail.com>
2024-01-11 02:52:16 -0300

    alterações iniciais

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,37 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 main() {
-  runApp(const AppWidget(
-    title: 'teste03',
-  ));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.red),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text('teste5')),
-    );
-  }
+  runApp(AppWidget());
 }
