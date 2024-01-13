f2be52979fab957bc0a67c56a57a3a324d9cff3c
Leonardo Lucena <leo.lucena10@hotmail.com>
2024-01-13 18:16:43 -0300

    alterações

--- my_app/lib/app_widget.dart
+++ my_app/lib/app_widget.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/login_page.dart';
 
-import 'home_page.dart';
-
 class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.red),
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
--- my_app/lib/login_page.dart
+++ my_app/lib/login_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController senhaController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,26 +22,50 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const TextField(
+              TextField(
+                controller: emailController,
                 keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'Email', border: OutlineInputBorder()),
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  // Adicione a lógica necessária aqui
+                  emailController.text = text;
+                },
               ),
               const SizedBox(height: 10),
-              const TextField(
+              TextField(
+                controller: senhaController,
                 obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Senha', border: OutlineInputBorder()),
+                decoration: const InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  // Adicione a lógica necessária aqui
+                  senhaController.text = text;
+                },
               ),
               const SizedBox(height: 15),
               ElevatedButton(
+                onPressed: _entrar,
                 child: const Text('Entrar'),
-                onPressed: () {},
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _entrar() {
+    String email = emailController.text;
+    String senha = senhaController.text;
+    if (email == 'leo' && senha == '123') {
+      print('correto');
+    }
+    // Adicione a lógica para autenticação ou processamento dos dados de login
+    print('Email: $email, Senha: $senha');
   }
 }
