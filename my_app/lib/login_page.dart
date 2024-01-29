11ca759febff4df1b93737c88adf4c201dda9319
Leonardo Lucena <leo.lucena10@hotmail.com>
2024-01-29 01:58:57 -0300

    alterações pontuais na tela de login com navegação de rotas

--- my_app/lib/login_page.dart
+++ my_app/lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,46 +14,48 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (text) {
+                    // Adicione a lógica necessária aqui
+                    emailController.text = text;
+                  },
                 ),
-                onChanged: (text) {
-                  // Adicione a lógica necessária aqui
-                  emailController.text = text;
-                },
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: senhaController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: senhaController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (text) {
+                    // Adicione a lógica necessária aqui
+                    senhaController.text = text;
+                  },
                 ),
-                onChanged: (text) {
-                  // Adicione a lógica necessária aqui
-                  senhaController.text = text;
-                },
-              ),
-              const SizedBox(height: 15),
-              ElevatedButton(
-                onPressed: _entrar,
-                child: const Text('Entrar'),
-              ),
-            ],
+                const SizedBox(height: 15),
+                ElevatedButton(
+                  onPressed: _entrar,
+                  child: const Text('Entrar'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -64,6 +67,8 @@ class _LoginPageState extends State<LoginPage> {
     String senha = senhaController.text;
     if (email == 'leo' && senha == '123') {
       print('correto');
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomePage()));
     }
     // Adicione a lógica para autenticação ou processamento dos dados de login
     print('Email: $email, Senha: $senha');
